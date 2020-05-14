2e7022eb7453dd7f86ceaf60313b8a80a56bb99f
Martijn Wip <flyingwip@gmail.com>
2020-05-14 13:26:32 +0200

    Add unit tests for private api endpoints

--- app/user/tests/test_users_api.py
+++ app/user/tests/test_users_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -106,3 +107,52 @@ class PublicUserApiTests(TestCase):
             TOKEN_URL, {'email': '', 'password': 'testpass'})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUsersApiTests(TestCase):
+    """Tests API requests that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@somedomain.com',
+            password='thisisverysecret123',
+            name='Ferry Phonny'
+        )
+
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profilee for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.data, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'email': self.user.email,
+            'name': self.user.name
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that post is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for logged in users"""
+        payload = {
+            'name': 'new name',
+            'password': 'newpass123'
+        }
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['passwords']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
